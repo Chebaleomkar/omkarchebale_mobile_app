ace5d08711e2b4e88a7abefa4e028fbe9250edbd
Omkar Chebale <omkarchebale0@gmail.com>
2025-06-27 00:21:31 +0530

    install nativeWind and configuring everythign to smoothly run the app

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -92,6 +92,7 @@ export default function TabTwoScreen() {
           ),
         })}
       </Collapsible>
+      
     </ParallaxScrollView>
   );
 }
---
+++ data.ts
@@ -0,0 +1,385 @@
+
+export const Bio = {
+    name: "Omkar Chebale ",
+    roles: [
+        "Helping businesses go digital with smart, custom web solutions",
+        "You can call me Rushi",
+        "Freelance Full-Stack Developer",
+        "MERN Stack Specialist",
+        "Digital Solution Builder",
+    ],
+    description: "I’m a passionate software engineer who helps businesses bring their digital ideas to life. From scalable web apps to custom solutions, I focus on clean code, great UX, and real results. I’ve worked with local businesses to automate systems, improve customer experience, and grow their online presence.",
+    github: "https://github.com/Chebaleomkar",
+    resume:
+        "https://drive.google.com/file/d/1_xD17Kg6MS0T0ahDAxltGXSxsccutMxr/view?usp=drive_link",
+    linkedin: "https://www.linkedin.com/in/omkar-chebale-8b251726b/",
+    twitter: "https://twitter.com/chebalerushi",
+    insta: "https://www.instagram.com/chebalerushi/",
+    facebook: "https://www.facebook.com/rushi.chebale.1",
+    stack_overflow: "https://stackoverflow.com/users/21063836/omkar-chebale",
+};
+
+export const skills = [
+    {
+        title: "Frontend",
+        skills: [
+            {
+                name: "HTML",
+                image: "/html.jpg",
+            },
+            {
+                name: "CSS",
+                image:
+                    "/css.jpg",
+            },
+            {
+                name: "Tailwind UI",
+                image:
+                    "/tailwind.jpg",
+            },
+            {
+                name: "Saas",
+                image:
+                    "/sass.png",
+            },
+            {
+                name: "JavaScript",
+                image:
+                    "/JavaScript.png",
+            },
+            {
+                name: "TypeScript",
+                image:
+                    "/typescript.png",
+            },
+            {
+                name: "React Js",
+                image:
+                    "/react.jpeg",
+            },
+            {
+                name: "Next Js",
+                image:
+                    "/next.jpeg",
+            },
+            {
+                name: "Astro Js",
+                image:
+                    "/astro.jpeg",
+            },
+        ],
+    },
+    {
+        title: "Backend",
+        skills: [
+            {
+                name: "Node Js",
+                image: "/node.jpeg",
+            },
+            {
+                name: "Express Js",
+                image:
+                    "/express.jpeg",
+            },
+            {
+                name: "Hono Js",
+                image: "/hono.jpeg",
+            },
+            {
+                name: "MongoDB",
+                image:
+                    "/mongodb.jpeg",
+            },
+            {
+                name: "MySQL",
+                image:
+                    "/mysql.jpeg",
+            },
+            {
+                name: "Postgresql",
+                image: "/postgres.jpeg",
+            },
+
+        ],
+    },
+    {
+        title: "Others",
+        skills: [
+            {
+                name: "Git",
+                image:
+                    "/git.jpeg",
+            },
+            {
+                name: "GitHub",
+                image:
+                    "/github.jpeg",
+            },
+
+            {
+                name: "Netlify",
+                image:
+                    "/netlify.jpeg",
+            },
+            {
+                name: "vercel",
+                image:
+                    "/vercel.jpeg",
+            },
+            {
+                name: "VS Code",
+                image:
+                    "/vscode.jpeg",
+            },
+            {
+                name: "Postman",
+                image:
+                    "/postman.jpeg",
+            },
+
+        ],
+    },
+];
+
+export const experiences = [
+
+    {
+        id: 0,
+        img: "https://avatars.githubusercontent.com/u/152776938?v=4",
+        role: " Fullstack Engineering Intern",
+        company: "Recursive Zero Pvt. Ltd.",
+        date: "jan 2024 - present",
+        desc: "Working on the frontend of the web application using ASTROJS",
+        skills: [
+            "JavaScript",
+            "TypeScript",
+            "Node Js",
+            "Astro Js",
+            "Tailwind UI",
+            "React Js",
+            "Node Js",
+        ],
+    },
+
+];
+
+export const education = [
+    {
+        id: 0,
+        img: "/rsml.jpeg",
+        school: "Rajarshi Shahu Mahavidyalaya, Latur (Autonomous)",
+        date: "july 2022 - Apr 2025",
+        // grade: "8.71 CGPA",
+        desc: "I am currently pursuing a Bachelor's degree of Computer Application (BCA) at Rajarshi Shahu Mahavidyalaya, Latur (Autonomous). I have completed 4 semesters and have a CGPA of 8.5 . I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks,Linux os  among others. ",
+        degree: "Bachelor of Computer Applications- BCA , Computer Science and Engineering",
+    },
+    {
+        id: 1,
+        img: "/dummyschool.jpeg",
+        school: "SRI ANANTPAL NUTAN VIDYALY High School, SHIRUR ANANTPAL",
+        date: "Apr 2020 - Apr 2022",
+        grade: "73.17%",
+        desc: "I completed my class 12 high school education at SRI ANANTPAL NUTAN VIDYALY High School, SHIRUR ANANTPAL where I studied Science (PCB) ",
+        degree: "HSC",
+    },
+    {
+        id: 2,
+        img: "/dummyschool.jpeg",
+        school: "Shri Lal Bahadur Shastri High School , Latur",
+        date: "june 2019 - Mar 2020",
+        grade: "89.4%",
+        desc: "I completed my class 10 education at Shri Lal Bahadur Shastri High School , Lautur ",
+        degree: "SSC",
+    },
+];
+
+export const projects = [
+    {
+        id: 0,
+        title: "DisciplineX : Discipline management app ",
+        TitleColor: "#f97316",
+        date: "Jun 2023 - Jul 2023",
+        description:
+            "DisciplineX is a digital tool designed to help institutions maintain discipline and promote positive behavior among students. Our app offers a comprehensive solution for managing disciplinary actions ,encouraging good behavior, and streamlining administrative processes. By leveraging technology, DisciplineX aims to create a more organized, efficient, and positive environment in educational institutions.",
+        image:
+            "/dx.jpeg",
+        tags: [
+            "NextJS",
+            "shadcn UI",
+            "JWT",
+            "cryptoJs",
+            "nodemailer",
+            "Zod",
+            "Typescript",
+            "Zustand",
+            "mongodb"
+        ],
+        category: "web app",
+        github: "https://github.com/Chebaleomkar/dx-full-stack",
+        webapp: "https://disciplinexin.vercel.app",
+    },
+    {
+        id: 10,
+        title: "spotify clone",
+        TitleColor: "#1db954",
+        date: "Jun 2023 - Jul 2023",
+        description:
+            "Build a Full Stack Spotify Clone with Next 13.4, React, Tailwind, Supabase, PostgreSQL . In the project complete music streaming application from scratch, replicating the popular features and functionalities of Spotify.",
+        image:
+            "/spotify.jpeg",
+        tags: [
+            "NextJS",
+            "Supabase",
+            "React hook form",
+            "Zod",
+            "Typescript",
+            "Zustand",
+            "PostgreSQL"
+        ],
+        category: "web app",
+        github: "https://github.com/Chebaleomkar/Spotify-clone-",
+        webapp: "https://omkar-chebale-spotify-clone.vercel.app/",
+    },
+    {
+        id: 1,
+        title: "Netflix clone",
+        date: "Jan 2024 - feb 2024",
+        TitleColor: "#E50914",
+
+        description:
+            "Build a Full Stack Netflix Clone with Next React, Tailwind, prisma  , mongoDb , Typescript . In the project complete music streaming application from scratch, replicating the popular features and functionalities of Netflix.",
+        image:
+            "/netflix.png",
+        tags: [
+            "Next Js",
+            "MogoDB",
+            "Typescript",
+            "Zod",
+            "Zustand",
+            "Tailwind UI ",
+            "ESLint",
+        ],
+        category: "web app",
+        github: "https://github.com/Chebaleomkar/Netlfix-clone"
+    },
+    {
+        id: 12,
+        title: "Zoom clone",
+
+        date: "Jan 2024 - feb 2024",
+        TitleColor: "blue",
+
+        description:
+            "This Zoom clone app is built using Next.js and offers a range of features to facilitate virtual meetings and collaborations. It provides secure authentication, real-time meeting controls, scheduling capabilities, and a responsive design for optimal user experience across devices.",
+        image:
+            "/zoom.jpeg",
+        tags: [
+            "Next Js",
+            "Typescript",
+            "Tailwind UI ",
+            "stream-io",
+            "ESLint",
+        ],
+        category: "web app",
+        github: "https://github.com/Chebaleomkar/zoom-clone",
+        webapp: "https://zoom-clone-chebaleomkars-projects.vercel.app/"
+    },
+    {
+        id: 2,
+        title: "YouTube Clone",
+        TitleColor: "#FF0000",
+        date: "Apr 2023 - May 2023",
+        description:
+            " Build a Full Stack YouTube Clone with  React, Tailwind . Developed full stack  web application that allows users to search for, play, and pause their favorite videos on demand and also added payment integration with stripe . For backend use of NodeJs for api requests  ",
+        image:
+            "/youtube.png",
+        tags: [
+            "React Js",
+            "Stripe ",
+            "Node Js",
+            "Express Js",
+            "Tailwind UI"
+        ],
+        category: "web app",
+        github: "https://github.com/Chebaleomkar/youtube-clone",
+    },
+    {
+        id: 4,
+        title: "Movix",
+        TitleColor: "yellow",
+        date: "Jan 2023 - Mar 2023",
+        description:
+            "Movix - Your Ultimate Movie and TV Show Explorer Welcome to Movix, your one-stop web application for exploring the world of movies and TV shows. Whether you're looking for the latest blockbusters or hidden gems, Movix has you covered. Built with React, React Router, and Redux Toolkit, this app offers an immersive and user-friendly experience for movie and TV enthusiasts.",
+        image:
+            "/movix.jpeg",
+        tags: ["React Js", "Infinite Scroll", " TMDB Api", "redux", "sass", "axios"],
+        category: "machine learning",
+        github: "https://github.com/Chebaleomkar/movix",
+    },
+];
+
+export const blogs = [
+    {
+        title: "what why and how zod works?",
+        link: "https://medium.com/@omkarchebale0/what-why-and-how-zod-works-b022e3bd13ec",
+        topic: "zod"
+    },
+]
+
+export const TimeLineData = [
+    { year: 2022, text: "Started my journey" },
+    { year: 2023, text: "worked on my technical skills " },
+    { year: 2023, text: "Shared my projects with the world" },
+    { year: 2024, text: "Intern as developer in Recursive Zero" },
+    { year: 2025, text: "Freelancing and helping business to digitize" },
+];
+
+export const ClientProjects = [
+    {
+        business: "Prisha Motors, Latur",
+        project: "Automatic Quotation System",
+        description: "Created a system to generate and send vehicle quotations to customers via WhatsApp  in PDF format.",
+        year: 2024,
+        impact: "Reduced manual workload by 70% and improved customer response time.",
+    },
+    {
+        business: "IIB Institute, Latur",
+        project: "Question Bank System",
+        description: "Developed a system to store and manage questions with images, formulas, or chemical reactions, and track student performance.",
+        year: 2025,
+        impact: "Helps teachers analyze each student's strengths and weaknesses based on past exam performance.",
+    },
+    {
+        business: "Newaskar Automobiles,Sangamner ",
+        project: "Automatic Quotation System",
+        description: "Created a system to generate and send vehicle quotations to customers via WhatsApp  in PDF format.",
+        year: 2025,
+        impact: "Reduced manual workload by 70% and improved customer response time",
+    },
+    {
+        business: "Shree Shyam Bhoj Restaurant, Latur",
+        project: "Restaurant Website with Daily Menu Management",
+        description: "Built a website with an admin panel to easily update and share the daily food menu.",
+        year: 2025,
+        impact: "Customers can now check the daily menu online without needing to call the restaurant.",
+    }
+]
+
+export const clientReviews = [
+    {
+        review:
+            "Rushi delivered an efficient, well-structured software solution for Prisha Motors. His technical skills, problem-solving mindset, and timely execution were impressive. We're very pleased with his work and look forward to future collaborations.",
+        by: "Amit Gilda",
+        profession: "MD - Prisha Motors",
+        rating: 5,
+    },
+
+    {
+        review:
+            "Rushi built a powerful Question Bank System for IIB Institute that made managing exam content—like formulas and diagrams extremely easy. The platform's ability to analyze individual student performance has been a game-changer for our faculty. His work was thoughtful, reliable, and impactful.",
+        by: "Shyamsundar Soni",
+        profession: "IIB Institute, Latur",
+        rating: 5,
+    },
+
+]
